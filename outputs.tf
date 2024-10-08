0f3a3aff6cee570589c22b7add0143e58da88594
Rohith Jayawardene <gambol99@gmail.com>
2024-10-08 10:08:11 +0100

    fix: the reference to the kms key was incorrect

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,12 @@
 
 output "kms_key_id" {
   description = "The KMS key ID used for the nuke service, if created"
-  value       = var.create_kms_key ? aws_kms_key.current.key_id : null
+  value       = var.create_kms_key ? module.kms[0].key_id : null
+}
+
+output "kms_key_arn" {
+  description = "The KMS key ARN used for the nuke service, if created"
+  value       = var.create_kms_key ? module.kms[0].key_arn : null
 }
 
 output "parameter_store_arn" {
@@ -18,4 +23,3 @@ output "vpc_id" {
   description = "The VPC where the nuke service is running"
   value       = local.vpc_id
 }
-
