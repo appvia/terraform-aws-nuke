4791a127ef8741e50688bafbfd23012b34031230
Rohith Jayawardene <gambol99@gmail.com>
2024-10-08 09:28:45 +0100

    chore: adding the kms key id to the outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 
+output "kms_key_id" {
+  description = "The KMS key ID used for the nuke service, if created"
+  value       = var.create_kms_key ? aws_kms_key.current.key_id : null
+}
+
 output "parameter_store_arn" {
   description = "The ARN of the parameter store containing the nuke configuration"
   value       = aws_ssm_parameter.configuration.arn
