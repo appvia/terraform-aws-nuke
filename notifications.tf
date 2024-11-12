83407e55597d985f11bbf3a1296a95a58047dfc8
Rohith Jayawardene <gambol99@gmail.com>
2024-11-12 09:06:52 +0000

    chore: fixing the reference to the notification lambda module

--- notifications.tf
+++ notifications.tf
@@ -75,7 +75,7 @@ resource "aws_lambda_permission" "allow_eventbridge" {
 
   statement_id  = "AllowExecutionFromEventBridge"
   action        = "lambda:InvokeFunction"
-  function_name = module.notifications[each.key].lambda_function_name
+  function_name = module.notification[each.key].lambda_function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.ecs_task_stopped_rule[each.key].arn
 }
@@ -84,6 +84,6 @@ resource "aws_lambda_permission" "allow_eventbridge" {
 resource "aws_cloudwatch_event_target" "invoke_lambda" {
   for_each = local.tasks_with_notifications
 
-  arn  = module.notifications[each.key].lambda_function_arn
+  arn  = module.notification[each.key].lambda_function_arn
   rule = aws_cloudwatch_event_rule.ecs_task_stopped_rule[each.key].name
 }
